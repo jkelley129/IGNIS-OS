3c51c9310cbeef36c0c4aebb7ccca55fb2e3a75d
Josh Kelley <josh9kelley@gmail.com>
2025-10-22 18:28:54 -0400

    Add memcpy() to string.c/h

--- libc/string.c
+++ libc/string.c
@@ -1,24 +1,22 @@
 #include "string.h"
 #include "stdint.h"
 
-// Using size_t (which is 64-bit in 64-bit mode)
-size_t strlen(const char* str){
+size_t strlen(const char* str) {
     size_t len = 0;
-    while(str[len]){
+    while (str[len]) {
         len++;
     }
     return len;
 }
 
-int strcmp(const char* s1, const char* s2){
-    while(*s1 && (*s1 == *s2)){
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
         s1++;
         s2++;
     }
     return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
-// Compare n characters of two strings
 int strncmp(const char* s1, const char* s2, size_t n) {
     while (n && *s1 && (*s1 == *s2)) {
         s1++;
@@ -41,7 +39,6 @@ char* strcpy(char* dest, const char* src) {
     return orig_dest;
 }
 
-// Copy n characters
 char* strncpy(char* dest, const char* src, size_t n) {
     char* orig_dest = dest;
     while (n && *src) {
@@ -56,24 +53,18 @@ char* strncpy(char* dest, const char* src, size_t n) {
 }
 
 char* strcat(char* dest, const char* src) {
-    char *original_dest = dest; // Store the original destination pointer to return it
+    char* original_dest = dest;
 
-    // Find the end of the destination string
     while (*dest != '\0') {
         dest++;
     }
 
-    // Copy characters from source to destination
     while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
+        *dest++ = *src++;
     }
 
-    // Add the null terminator to the end of the concatenated string
     *dest = '\0';
-
-    return original_dest; // Return the pointer to the original destination
+    return original_dest;
 }
 
 void* memset(void* s, int c, size_t n) {
@@ -82,6 +73,16 @@ void* memset(void* s, int c, size_t n) {
         *p++ = (unsigned char)c;
     }
     return s;
+}
+
+// --- Added memcpy implementation ---
+void* memcpy(void* dest, const void* src, size_t n) {
+    unsigned char* d = dest;
+    const unsigned char* s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
 }
 
 void uitoa(uint64_t value, char* str) {
--- libc/string.h
+++ libc/string.h
@@ -11,6 +11,7 @@ char* strcpy(char* dest, const char* src);
 char* strncpy(char* dest, const char* src, size_t n);
 char* strcat(char* dest, const char* source);
 void* memset(void* s, int c, size_t n);
+void* memcpy(void* dest, const void* src, size_t n);
 void uitoa(uint64_t value, char* str);
 
 #endif
